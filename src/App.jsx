f21f646151a8e34c7f797fc6cc38143197616c69
Sebastian-Herrera <herrera6911sebastian@hotmail.com>
2022-05-15 19:23:47 -0500

    feat: nextui components

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,19 @@
 import React, { useState } from 'react';
-import { registerSW } from 'virtual:pwa-register';
+import { NextUIProvider } from '@nextui-org/react';
 
 import ReloadPrompt from './components/ReloadPrompt/ReloadPrompt';
 import DrawSignature from './components/DrawSignature/DrawSignature';
 
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  // const updateSW = registerSW({
-  //   onRegistered(r) {
-  //     r &&
-  //       setInterval(() => {
-  //         r.update()
-  //           .then(() => {
-  //             console.log('buscando');
-  //           })
-  //           .catch((err) => {
-  //             // console.log('error::::::' + err);
-  //             console.log('sin conexion');
-  //           });
-  //       }, 2000);
-  //   },
-  //   onOfflineReady() {
-  //     setInterval(() => {
-  //       r.update()
-  //         .then(() => {
-  //           location.reload();
-  //           console.log('nuevo service');
-  //         })
-  //         .catch((err) => {
-  //           // console.log('error::::::' + err);
-  //           console.log('sin conexion');
-  //         });
-  //     }, 2000);
-  //   },
-  // });
-
   return (
-    <>
+    <NextUIProvider>
       <ReloadPrompt />
       <div className='App'>
-        <DrawSignature/>
+        <DrawSignature />
       </div>
-    </>
+    </NextUIProvider>
   );
 }
 
