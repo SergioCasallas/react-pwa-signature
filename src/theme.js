7b2e7ce39876c1aa4d4f4f7151cc37081146c3bd
Sebastian-Herrera <herrera6911sebastian@hotmail.com>
2022-05-15 21:17:17 -0500

    feat: nextui components

--- src/theme.js
+++ src/theme.js
@@ -1,4 +1,13 @@
 // https://github.com/nextui-org/nextui/blob/main/packages/react/src/theme/dark-theme.ts
+
 export default {
   type: 'dark',
+  // className: 'dark-retro',
+  // theme: {
+  //   colors: {
+  //     primary: '#FFD34E',
+  //     primaryLight: 'transparent',
+  //     error: '#EE457E',
+  //   },
+  // },
 };
